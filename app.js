2ef7af609d901064efe002308379cf36f2670443
LuisVad <20213tn002@utez.edu.mx>
2024-02-01 18:40:59 -0600

    Ya crea mascotas aleatorias

--- app.js
+++ app.js
@@ -21,4 +21,71 @@ const createClient = () => {
         });
 }
 
-setInterval(createClient,5000);
+// INSERT INTO `vet`.`pets`
+// (`id`,
+// `name`,
+// `type`,
+// `breed`,
+// `owner_id`,
+// `created_at`)
+// VALUES
+// (<{id: }>,
+// <{name: }>,
+// <{type: }>,
+// <{breed: }>,
+// <{owner_id: }>,
+// <{created_at: CURRENT_TIMESTAMP}>);
+
+const tipoMasco = ['Perro', 'Gato'];	
+const razaPerro = ['Labrador', 'Pastor Alemán', 'Bulldog', 'Beagle', 'Pitbull', 'Chihuahua'];
+const razaGato = ['Siamés', 'Persa', 'Bengala', 'Ragdoll', 'Sphynx ', 'Maine Coon'];
+
+
+const getAllOwners = () => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT id FROM clients';
+        db.query(sql, (err, results) => {
+            if (err) reject(err);
+            resolve(results);
+        });
+    });
+}
+
+const getRandomOwner = async () => {
+    const owners = await getAllOwners();
+    return owners[Math.floor(Math.random() * owners.length)].id;
+}
+
+const getRandomType = () => {
+    return tipoMasco[Math.floor(Math.random() * tipoMasco.length)];
+}
+
+const getRandomBreed = (type) => {
+    switch (type) {
+        case 'Perro':
+            return razaPerro[Math.floor(Math.random() * razaPerro.length)];
+        case 'Gato':
+            return razaGato[Math.floor(Math.random() * razaGato.length)];
+        default:
+            return 'N/A';
+    }
+}
+
+const createRandomPet = async () => {
+    try {
+        const owner_id = await getRandomOwner();
+        const type = getRandomType();
+        const breed = getRandomBreed(type);
+        const response = await axios.get('https://randomuser.me/api/');
+        const { name } = response.data.results[0];
+        const sql = `INSERT INTO pets (name, type, breed, owner_id, created_at) VALUES ('${name.first}', '${type}', '${breed}', ${owner_id}, '2024-02-01 19:07:41')`;
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log('Mascota creada!');
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+setInterval(createRandomPet, 5000);
